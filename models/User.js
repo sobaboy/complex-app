e63b89f5ed93624bce454c9345cbd2b534dc53b0
sobaboy <1214hso@naver.com>
2022-10-15 19:47:17 +0900

    adding register check

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const bcrypt = require("bcryptjs");
+const { resolve } = require("path");
 const usersCollection = require("../db").db().collection("users");
 const validator = require("validator");
 class User {
@@ -24,33 +25,58 @@ class User {
     };
   }
   validate() {
-    if (this.data.username == "") {
-      this.errors.push("이름을 추가 해주세요");
-    }
-    if (
-      this.data.username != "" &&
-      !validator.isAlphanumeric(this.data.username)
-    ) {
-      this.errors.push("사용자이름은 문자와 숫자만 포함할 수 있습니다");
-    }
-    if (!validator.isEmail(this.data.email)) {
-      this.errors.push("유효한 이메일을 추가 해주세요");
-    }
-    if (this.data.password == "") {
-      this.errors.push("비밀번호를 추가 해주세요");
-    }
-    if (this.data.password.length > 0 && this.data.password.length < 8) {
-      this.errors.push("비밀번호는 최소 8자 이상이어야 합니다.");
-    }
-    if (this.data.password.length > 30) {
-      this.errors.push("비밀번호는 30자를 초과할 수 없습니다.");
-    }
-    if (this.data.username.length > 0 && this.data.username.length < 2) {
-      this.errors.push("사용자이름은 최소 2글자 이상이어야 합니다.");
-    }
-    if (this.data.username.length > 30) {
-      this.errors.push("사용자이름는 30글자를 초과할 수 없습니다.");
-    }
+    return new Promise(async (resolve, reject) => {
+      if (this.data.username == "") {
+        this.errors.push("이름을 추가 해주세요");
+      }
+      if (
+        this.data.username != "" &&
+        !validator.isAlphanumeric(this.data.username)
+      ) {
+        this.errors.push("사용자이름은 문자와 숫자만 포함할 수 있습니다");
+      }
+      if (!validator.isEmail(this.data.email)) {
+        this.errors.push("유효한 이메일을 추가 해주세요");
+      }
+      if (this.data.password == "") {
+        this.errors.push("비밀번호를 추가 해주세요");
+      }
+      if (this.data.password.length > 0 && this.data.password.length < 8) {
+        this.errors.push("비밀번호는 최소 8자 이상이어야 합니다.");
+      }
+      if (this.data.password.length > 30) {
+        this.errors.push("비밀번호는 30자를 초과할 수 없습니다.");
+      }
+      if (this.data.username.length > 0 && this.data.username.length < 2) {
+        this.errors.push("사용자이름은 최소 2글자 이상이어야 합니다.");
+      }
+      if (this.data.username.length > 30) {
+        this.errors.push("사용자이름는 30글자를 초과할 수 없습니다.");
+      }
+      // username이 valid 할 경우에만 그 다음에 DB로 접근해서 이미 존재하는 name 인지 check하도록
+      if (
+        this.data.username.length > 2 &&
+        this.data.username.length < 31 &&
+        validator.isAlphanumeric(this.data.username)
+      ) {
+        let usernameExists = await usersCollection.findOne({
+          username: this.data.username,
+        });
+        if (usernameExists) {
+          this.errors.push("이미 사용 중인 사용자이름입니다");
+        }
+      }
+      // email
+      if (validator.isEmail(this.data.email)) {
+        let emailExists = await usersCollection.findOne({
+          email: this.data.email,
+        });
+        if (emailExists) {
+          this.errors.push("이미 사용 중인 E-mail 입니다");
+        }
+      }
+      resolve();
+    });
   }
 
   login() {
@@ -73,17 +99,23 @@ class User {
         });
     });
   }
+
   register() {
-    // Step #1 : Validate user data
-    this.cleanUp();
-    this.validate();
-    // Step #2: Only if there are no validation errors then save the user data into database
-    if (!this.errors.length) {
-      // hash user password
-      let salt = bcrypt.genSaltSync(10);
-      this.data.password = bcrypt.hashSync(this.data.password, salt);
-      usersCollection.insertOne(this.data);
-    }
+    return new Promise(async (resolve, reject) => {
+      // Step #1 : Validate user data
+      this.cleanUp();
+      await this.validate();
+      // Step #2: Only if there are no validation errors then save the user data into database
+      if (!this.errors.length) {
+        // hash user password
+        let salt = bcrypt.genSaltSync(10);
+        this.data.password = bcrypt.hashSync(this.data.password, salt);
+        await usersCollection.insertOne(this.data);
+        resolve();
+      } else {
+        reject(this.errors);
+      }
+    });
   }
 }
 
