602ec82d68836c8f952092b1829eef438c771aa6
sobaboy <1214hso@naver.com>
2022-10-17 23:58:28 +0900

    implementing delete post

--- controllers/postController.js
+++ controllers/postController.js
@@ -28,16 +28,13 @@ exports.viewSingle = async function (req, res) {
 
 exports.viewEditScreen = async function (req, res) {
   try {
-    let post = await Post.findSingleById(req.params.id);
-    if (post.authorId == req.visitorId) {
+    let post = await Post.findSingleById(req.params.id, req.visitorId);
+    if (post.isVisitorOwner) {
       res.render("edit-post", { post: post });
     } else {
-      req.flash("errors", "권한이 없습니다");
-      req.session.save(() => {
-        res.redirect("/");
-      });
+      req.flash("errors", "You do not have permission to perform that action.");
+      req.session.save(() => res.redirect("/"));
     }
-    res.render("edit-post", { post: post });
   } catch {
     res.render("404");
   }
@@ -67,5 +64,19 @@ exports.edit = function (req, res) {
       req.session.save(function () {
         res.redirect("/");
       });
+    });
+};
+
+exports.delete = function (req, res) {
+  Post.delete(req.params.id, req.visitorId)
+    .then(() => {
+      req.flash("success", "포스트가 삭제되었습니다");
+      req.session.save(() => {
+        res.redirect(`/profile/${req.session.user.username}`);
+      });
+    })
+    .catch(() => {
+      req.flash("errors", "삭제할 권한이 없습니다");
+      req.session.save(() => res.redirect("/"));
     });
 };
