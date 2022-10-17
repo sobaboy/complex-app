f8239fcee26a29b641a316d240714717ef7de26d
sobaboy <1214hso@naver.com>
2022-10-17 18:47:56 +0900

    adding posts by author

--- models/Post.js
+++ models/Post.js
@@ -55,43 +55,51 @@ class Post {
   }
 }
 
+Post.reusablePostQuery = function (uniqueOperations) {
+  return new Promise(async function (resolve, reject) {
+    let aggOperations = uniqueOperations.concat([
+      {
+        $lookup: {
+          from: "users",
+          localField: "author",
+          foreignField: "_id",
+          as: "authorDocument",
+        },
+      },
+      {
+        $project: {
+          title: 1,
+          body: 1,
+          createdDate: 1,
+          author: { $arrayElemAt: ["$authorDocument", 0] },
+        },
+      },
+    ]);
+    let posts = await postsCollection.aggregate(aggOperations).toArray();
+
+    // clean up author property in each post object
+    posts = posts.map(function (post) {
+      post.author = {
+        username: post.author.username,
+        avatar: new User(post.author, true).avatar,
+      };
+
+      return post;
+    });
+
+    resolve(posts);
+  });
+};
+
 Post.findSingleById = function (id) {
   return new Promise(async function (resolve, reject) {
     if (typeof id != "string" || !ObjectId.isValid(id)) {
       reject();
       return;
     }
-    let posts = await postsCollection
-      .aggregate([
-        { $match: { _id: new ObjectId(id) } },
-        {
-          $lookup: {
-            from: "users",
-            localField: "author",
-            foreignField: "_id",
-            as: "authorDocument",
-          },
-        },
-        // {
-        //   $project: {
-        //     title: 1,
-        //     body: 1,
-        //     createdDate: 1,
-        //     author: { $arrayElemAt: ["$authorDocument", 0] },
-        //   },
-        // },
-      ])
-      .toArray();
-
-    // // clean up author property in each post object
-    // posts = posts.map(function (post) {
-    //   post.author = {
-    //     username: post.author.username,
-    //     avatar: new User(post.author, true).avatar,
-    //   };
-
-    //   return post;
-    // });
+    let posts = await Post.reusablePostQuery([
+      { $match: { _id: new ObjectID(id) } },
+    ]);
 
     if (posts.length) {
       console.log(posts[0]);
@@ -100,6 +108,13 @@ Post.findSingleById = function (id) {
       reject();
     }
   });
+};
+
+Post.findByAuthorId = function (authorId) {
+  return Post.reusablePostQuery([
+    { $match: { author: authorId } },
+    { $sort: { createdDate: -1 } },
+  ]);
 };
 
 module.exports = Post;
